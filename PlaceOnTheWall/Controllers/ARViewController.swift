184f359d8a7793c235d30e509ae9599aa42f8903
Olena Stepaniuk <helen.stepaniuk@gmail.com>
2021-03-08 22:45:39 +0200

    rebuild logic

--- PlaceOnTheWall/Controllers/ARViewController.swift
+++ PlaceOnTheWall/Controllers/ARViewController.swift
@@ -9,132 +9,96 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ARViewController: UIViewController, ARSessionDelegate {
+class ARViewController: UIViewController {
     
     @IBOutlet weak var sceneView: ARSCNView!
     
+    let sceneManager = SceneManager()
+    
     var currentAngleY: Float = 0.0
-    var previousLoc = CGPoint.init(x: 0, y: 0)
-    var isRotating = false
     
-    var paintingNumber = 0
+    var touchBeganTime: Date?
     
-    var currentNode: SCNNode? {
-        get {
-            return sceneView.scene.rootNode.childNode(withName: "painting\(paintingNumber)", recursively: false)
+    var paintingNumber = 0 {
+        didSet {
+            sceneManager.paintingNumber = paintingNumber
         }
     }
     
+    var tileHasMoved = false
+    var draggedNode: SCNNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("painting number \(paintingNumber)")
-        initSceneView()
-        initScene()
-        initARSession()
-        loadModels()
+        
+        sceneManager.attach(to: sceneView)
+        sceneManager.showSceneDebugInfo()
         
         let scaleGesture = UIPinchGestureRecognizer(target: self, action: #selector(scaleNode))
         self.sceneView.addGestureRecognizer(scaleGesture)
         
         let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(rotateNode))
         self.sceneView.addGestureRecognizer(rotationGesture)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touches.forEach {
+            let touch = $0
+            if(touch.view == self.sceneView) {
+                let viewTouchLocation = touch.location(in: sceneView)
+                let arHitTestResult = sceneView.hitTest(viewTouchLocation, types: .featurePoint)
+                guard arHitTestResult.count > 0 else {
+                    return
+                }
+                
+                let result = sceneView.hitTest(viewTouchLocation, options: [SCNHitTestOption.searchMode: SCNHitTestSearchMode.all.rawValue])
+                guard !result.isEmpty else {
+                    return
+                }
+                
+                tileHasMoved = false
+                
+                for hitTestResult in result {
+                    let topNode = hitTestResult.node
+                        draggedNode = topNode
+                        
+                        touchBeganTime = Date()
+                        tileHasMoved = false
+                        return
+                }
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-        
-        self.sceneView.addGestureRecognizer(gestureRecognizer)
-    }
-    
-    @IBAction func upTapped(_ sender: Any) {
-        guard let currentNode = currentNode else { return }
-        let moveUp = SCNAction.repeat(SCNAction.moveBy(x: 0, y: 0.05, z: 0, duration: 0.1), count: 1)
-        currentNode.runAction(moveUp)
-    }
-
-    @IBAction func downTapped(_ sender: Any) {
-        guard let currentNode = currentNode else { return }
-        let moveDown = SCNAction.repeat(SCNAction.moveBy(x: 0, y: -0.05, z: 0, duration: 0.1), count: 1)
-        currentNode.runAction(moveDown)
-    }
-    
-    @IBAction func leftTapped(_ sender: Any) {
-        guard let currentNode = currentNode else { return }
-        let moveLeft = SCNAction.repeat(SCNAction.moveBy(x: -0.05, y: 0, z: 0, duration: 0.1), count: 1)
-        currentNode.runAction(moveLeft)
-    }
-    
-    @IBAction func rightTapped(_ sender: Any) {
-        guard let currentNode = currentNode else { return }
-        let moveRight = SCNAction.repeat(SCNAction.moveBy(x: 0.05, y: 0, z: 0, duration: 0.1), count: 1)
-        currentNode.runAction(moveRight)
-    }
-    
-    @IBAction func forwardTapped(_ sender: Any) {
-        guard let currentNode = currentNode else { return }
-        let moveForward = SCNAction.repeat(SCNAction.moveBy(x: 0, y: 0, z: 0.05, duration: 0.1), count: 1)
-        currentNode.runAction(moveForward)
-    }
-    
-    @IBAction func backwardTapped(_ sender: Any) {
-        guard let currentNode = currentNode else { return }
-        let moveBackward = SCNAction.repeat(SCNAction.moveBy(x: 0, y: 0, z: -0.05, duration: 0.1), count: 1)
-        currentNode.runAction(moveBackward)
-    }
-    
-    @IBAction func rotateRightTapped(_ sender: Any) {
-        guard let currentNode = currentNode else { return }
-        let moveBackward = SCNAction.repeat(SCNAction.rotateBy(x: 0, y: CGFloat(0.1 * Double.pi), z: 0, duration: 0.1), count: 1)
-        currentNode.runAction(moveBackward)
-    }
-    
-    
-    // MARK: - Initialization
-    
-    func initSceneView() {
-        sceneView.delegate = self
-    }
-    
-    func initScene() {
-        let scene = SCNScene()
-        sceneView.scene = scene
-        scene.physicsWorld.speed = 1
-        //      scene.physicsWorld.timeStep = 1.0 / 60.0
-    }
-    
-    func initARSession() {
-        guard ARWorldTrackingConfiguration.isSupported else {
-            print("*** ARConfig: AR World Tracking Not Supported")
+        guard let touchBeganTime = touchBeganTime, Date().timeIntervalSince1970 -  touchBeganTime.timeIntervalSince1970 > 0.3 else {
             return
         }
-        
-        let sessionConfig: ARWorldTrackingConfiguration = ARWorldTrackingConfiguration()
-        sessionConfig.planeDetection = .vertical
-        sceneView.session.delegate = self
-        sceneView.debugOptions = .showFeaturePoints
-        sceneView.antialiasingMode = .multisampling4X
-        sceneView.automaticallyUpdatesLighting = true
-        sceneView.preferredFramesPerSecond = 60
-        
-        if let camera = sceneView.pointOfView?.camera {
-            camera.wantsHDR = true
-            camera.wantsExposureAdaptation = true
-            camera.exposureOffset = -1
-            camera.minimumExposure = -1
+        guard let draggedNode = draggedNode else { return }
+        touches.forEach {
+            let touch = $0
+            if touch.view == self.sceneView {
+                let viewTouchLocation = touch.location(in: sceneView)
+                let results = sceneView.hitTest(viewTouchLocation, types: .existingPlane)
+                guard results.count > 0 else { return }
+                draggedNode.simdWorldTransform = results[0].worldTransform
+                draggedNode.eulerAngles.x = -.pi/2
+                tileHasMoved = true
+            }
         }
-        sceneView.session.run(sessionConfig, options: [.resetTracking, .removeExistingAnchors])
-        sceneView.delegate = self
     }
     
-    func loadModels() {
-        guard let paintingScene = SCNScene(named: "Paintings.scnassets/Paintings/Painting\(paintingNumber).scn") else { return }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        guard let paintingNode = paintingScene.rootNode.childNode(withName: "painting\(paintingNumber)", recursively: false) else { return }
+        guard tileHasMoved else { return }
         
-        sceneView.scene.rootNode.addChildNode(paintingNode)
+        self.draggedNode = nil
     }
     
     @objc func scaleNode(gesture: UIPinchGestureRecognizer) {
         
-        guard let currentNode = currentNode else { return }
+        guard let currentNode = sceneManager.currentParentNode?.childNodes.first else { return }
         if gesture.state == .changed {
             
             let pinchScaleX: CGFloat = gesture.scale * CGFloat((currentNode.scale.x))
@@ -142,71 +106,22 @@ class ARViewController: UIViewController, ARSessionDelegate {
             let pinchScaleZ: CGFloat = gesture.scale * CGFloat((currentNode.scale.z))
             currentNode.scale = SCNVector3Make(Float(pinchScaleX), Float(pinchScaleY), Float(pinchScaleZ))
             gesture.scale = 1
-            
         }
-        if gesture.state == .ended { }
-        
     }
     
     @objc func rotateNode(_ gesture: UIRotationGestureRecognizer) {
         
-        guard let currentNode = currentNode else { return }
+        guard let currentNode = sceneManager.currentParentNode?.childNodes.first else { return }
         
         let rotation = Float(gesture.rotation)
         
         if gesture.state == .changed {
-            isRotating = true
             currentNode.eulerAngles.y = currentAngleY + rotation
         }
         
         if(gesture.state == .ended) {
             currentAngleY = currentNode.eulerAngles.y
-            isRotating = false
         }
     }
-    
-    @objc func handlePan(_ gesture: UIPanGestureRecognizer) {
-        guard let currentNode = currentNode else { return }
-        
-        if !isRotating{
-            
-            let currentTouchPoint = gesture.location(in: self.sceneView)
-            
-            guard let hitTest = self.sceneView.hitTest(currentTouchPoint, types: .existingPlane).first else { return }
-            
-            let worldTransform = hitTest.worldTransform
-            
-            let newPosition = SCNVector3(worldTransform.columns.3.x, worldTransform.columns.3.y, worldTransform.columns.3.z)
-            
-            currentNode.simdPosition = float3(newPosition.x, newPosition.y, newPosition.z)
-            
-        }
-    }
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
-}
 
-// MARK: - ARSCNViewDelegate
-extension ARViewController: ARSCNViewDelegate {
-    /*
-     // Override to create and configure nodes for anchors added to the view's session.
-     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-     let node = SCNNode()
-     
-     return node
-     }
-     */
 }
---
+++ PlaceOnTheWall/SceneManager.swift
@@ -0,0 +1,130 @@
+//
+//  SceneManager.swift
+//  PlaceOnTheWall
+//
+//  Created by Olena Stepaniuk on 08.03.2021.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+class SceneManager: NSObject {
+    
+    var sceneView: ARSCNView?
+    
+    var initVirtualTiles = false
+    var currentParentNode: SCNNode?
+    var paintingNumber: Int? 
+    
+    var grids = [Grid]()
+    
+    
+    func attach(to arSceneView: ARSCNView) {
+        guard ARWorldTrackingConfiguration.isSupported else {
+            print("*** ARConfig: AR World Tracking Not Supported")
+            return
+        }
+        
+        sceneView = arSceneView
+        sceneView?.delegate = self
+        sceneView?.session.delegate = self
+        
+        sceneView?.autoenablesDefaultLighting = true
+        sceneView?.scene.physicsWorld.gravity = SCNVector3(0, -3.0, 0)
+        
+        configureSceneView(self.sceneView!)
+    }
+    
+    func showSceneDebugInfo() {
+        sceneView?.debugOptions = .showFeaturePoints
+        sceneView?.showsStatistics = true
+    }
+    
+    private func configureSceneView(_ sceneView: ARSCNView) {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.vertical]
+        configuration.isLightEstimationEnabled = true
+        
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+        sceneView.antialiasingMode = .multisampling4X
+        sceneView.automaticallyUpdatesLighting = true
+        sceneView.preferredFramesPerSecond = 60
+    }
+    
+    func addPainting(to node: SCNNode) {
+        let planeGeometry = SCNPlane(width: 0.25, height: 0.25)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "painting\(paintingNumber ?? 0)")
+        planeGeometry.materials = [material]
+        
+        let paintingNode = SCNNode(geometry: planeGeometry)
+        paintingNode.eulerAngles = SCNVector3(paintingNode.eulerAngles.x + (-Float.pi / 2), paintingNode.eulerAngles.y, paintingNode.eulerAngles.z)
+        paintingNode.position = node.position
+        node.addChildNode(paintingNode)
+        
+        initVirtualTiles = true
+    }
+    
+    private func addNodeAnchor(worldTransform: simd_float4x4) {
+        sceneView?.session.add(anchor: ARAnchor(name: "node_anchor", transform: worldTransform))
+    }
+    
+    
+}
+
+extension SceneManager: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let name = anchor.name, !initVirtualTiles, name == "node_anchor" {
+            addPainting(to: node)
+            currentParentNode = node
+            self.grids.forEach { $0.removeFromParentNode() }
+            return
+        }
+        
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else { return }
+        if planeAnchor.extent.x * planeAnchor.extent.z > 0.1 {
+            addNodeAnchor(worldTransform: anchor.transform)
+            return
+        }
+        let grid = Grid(anchor: planeAnchor)
+        self.grids.append(grid)
+        node.addChildNode(grid)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else { return }
+        let grid = self.grids.filter { grid in
+            return grid.anchor.identifier == planeAnchor.identifier
+        }.first
+        
+        if planeAnchor.extent.x * planeAnchor.extent.z > 0.1 {
+            addNodeAnchor(worldTransform: anchor.transform)
+            return
+        }
+        
+        guard let foundGrid = grid else {
+            return
+        }
+        
+        foundGrid.update(anchor: planeAnchor)
+    }
+}
+
+extension SceneManager: ARSessionDelegate {
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
